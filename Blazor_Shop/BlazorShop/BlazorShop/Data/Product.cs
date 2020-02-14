5fc1d25733000c7d05f3aba4ef385a6d64ace31f
ahmedalalaf <ahmedalalaf@gmail.com>
2020-02-15 00:48:43 +0300

    Auth

--- Blazor_Shop/BlazorShop/BlazorShop/Data/Product.cs
+++ Blazor_Shop/BlazorShop/BlazorShop/Data/Product.cs
@@ -12,6 +12,7 @@ namespace BlazorShop.Data
         public int Id { get; set; }
         [Required]
         public string Name { get; set; }
+        [Required]
         [Range(0,500)]
         public double Price { get; set; }
         public byte[] Image { get; set; }
