7b7ed628483d8d9bff90f85058ca60d0c4ea4985
Ahmed Aalalaf <ahmedalalaf@gmail.com>
2020-02-18 17:12:24 +0300

    Add Details Page

---
+++ Blazor_Shop/BlazorShop/BlazorShop/Services/AppointmentService.cs
@@ -0,0 +1,25 @@
+﻿using BlazorShop.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorShop.Services
+{
+    public class AppointmentService
+    {
+        private readonly ApplicationDbContext _db;
+
+        public AppointmentService(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public bool CreateAppoinment(Appointment appointment)
+        {
+            _db.Add(appointment);
+            _db.SaveChanges();
+            return true;
+        }
+    }
+}
